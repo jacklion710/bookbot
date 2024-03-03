58d0755cee44b74038e7d6ff9365cddcc1c07abf
jacklion710 <jack.lion710@gmail.com>
2024-03-03 18:09:39 -0500

    Character count dictionary

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 def main():
-    path_to_file = "books/frankenstein.txt"
-
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        print(file_contents)
-
-def word_count():
-    path_to_file = "books/frankenstein.txt"
-
-    with open(path_to_file) as f:
-        file_contents = (f.read()).split()
-        count = len(file_contents)
-        print(count)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
 
 
-word_count()
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
